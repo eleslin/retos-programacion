e40326178675bda5f76c42b4d32c4d7d27fb9a88
eleslin <srta.scobar@gmail.com>
2025-08-28 19:14:21 +0200

    feat: exercise 0

---
+++ 00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/solucion.js
@@ -0,0 +1,39 @@
+/*  
+Crea un comentario en el código y coloca la URL del sitio web oficial del
+lenguaje de programación que has seleccionado.
+*/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+/*
+Representa las diferentes sintaxis que existen de crear comentarios
+ en el lenguaje (en una línea, varias...).
+*/
+// Single line comment
+/*
+Multi
+line
+comment 
+*/
+
+/* 
+Crea una variable (y una constante si el lenguaje lo soporta).
+*/
+let miVariable = 1
+const MI_CONSTANTE = 'test'
+
+/*
+Crea variables representando todos los tipos de datos primitivos
+*/
+let tipoString = 'Hola Mundo'
+let tipoNumber = 27
+let tipoBool = true
+let tipoNull = null
+let tipoUndefined
+let tipoSymbol = Symbol("mysymbol")
+let tipoBigInt = BigInt(2343424234242)
+
+/**
+ * Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ */
+console.log("¡Hola, JavaScript!")
+
